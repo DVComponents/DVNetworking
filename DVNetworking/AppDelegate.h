1a0ecdcf7a2c08f0debaf8124fcebafc83730898
wangning-lingli <351723770@qq.com>
2018-12-05 23:12:20 +0800

    Initial Commit

---
+++ DVNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DVNetworking
+//
+//  Created by 王宁 on 2018/12/5.
+//  Copyright © 2018年 王宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
